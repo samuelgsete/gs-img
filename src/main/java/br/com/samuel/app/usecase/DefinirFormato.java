b43d0b8e85db680c34a17b8fefebeec8dc5232ba
samuel <samuelgsete@gmail.com>
2022-10-28 15:59:40 -0300

    Corrigindo o bug de arquivos inválidos

--- src/main/java/br/com/samuel/app/usecase/DefinirFormato.java
+++ src/main/java/br/com/samuel/app/usecase/DefinirFormato.java
@@ -3,12 +3,12 @@ package br.com.samuel.app.usecase;
 import br.com.samuel.app.exception.model.FormatoNaoSuportado;
 
 class Jpeg {
-    public static final String TIPO = "imagem/jpeg";
+    public static final String TIPO = "image/jpeg";
     public static final String FORMATO = ".jpg";
 }
 
 class Png {
-    public static final String TIPO = "imagem/png";
+    public static final String TIPO = "image/png";
     public static final String FORMATO = ".png";
 }
 
