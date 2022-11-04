17f78f05b77023d1ea2d63f43a204bcadfcc72e4
samuel <samuelgsete@gmail.com>
2022-11-04 17:37:36 -0300

    Aumento da resolução suportada

--- src/main/java/br/com/samuel/app/model/ImagemPadrao.java
+++ src/main/java/br/com/samuel/app/model/ImagemPadrao.java
@@ -2,7 +2,7 @@ package br.com.samuel.app.model;
 
 public class ImagemPadrao {
     public static String FORMATOS_ACEITOS = "image/jpg, image/png, .png, .jpg, .jpeg";
-    public static String RESOLUCAO_SUPORTADA = "1000x1000";
-    public static Integer LARGURA_MAXIMA = 1000;
-    public static Integer ALTURA_MAXIMA = 1000;
+    public static String RESOLUCAO_SUPORTADA = "1400x1400";
+    public static Integer LARGURA_MAXIMA = 1400;
+    public static Integer ALTURA_MAXIMA = 1400;
 }
